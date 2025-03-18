7ef7e25a884b6f84f2f61270149153505d8ee67d
FuthifoXx <futhifoxmaseko@gmail.com>
2025-03-18 11:24:04 +0200

    second commit

--- src/products/Product.jsx
+++ src/products/Product.jsx
@@ -1,11 +1,4 @@
-function Product({
-  id,
-  productImage,
-  productTitle,
-  productDetails,
-  price,
-  status,
-}) {
+function Product() {
   return (
     <article>
       <img src={productImage} alt="" />
@@ -18,4 +11,52 @@ function Product({
   );
 }
 
-export default Product;
+// export default Product;
+// [6:56 PM] Ogbu Esther Uzoma
+// import React from "react";
+// import bag from "../assets/bag3.jpg"
+// const Card = () => {
+//   return (
+//     <div style={{ width: "300px", height: "300px", backgroundColor: "white" , border:"1px solid gray" , borderRadius : "15px"  , margin : "10px"}}>
+//       <div style={{width: "100%", height: "100px"}}>
+//         <img style={{objectFit: "contain" ,width : "100%" , height:"100%"}} src={bag} alt="product image" />
+//       </div>
+//       <div>
+//         <h3>
+//           Product title <span>Price</span>
+//         </h3>
+//         <p>
+//           Product details Lorem, ipsum dolor sit amet consectetur adipisicing
+//           elit. Optio, repudiandae. Deserunt, deleniti.
+//         </p>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Card;
+
+import React from 'react';
+import Card from './Card';
+
+const Products = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        flexWrap: 'wrap',
+        width: '100%',
+        height: '100vh',
+      }}
+    >
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+};
+
+export default Products;
